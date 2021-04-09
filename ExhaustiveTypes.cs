fb31f87f0969500e3b45e5decb304870cc664be6
Lars Rönnbäck <lars.ronnback@anchormodeling.com>
2021-04-10 00:40:03 +0200

    Removed unnecessary using
     (after switching from class to struct)

--- ExhaustiveTypes.cs
+++ ExhaustiveTypes.cs
@@ -2,7 +2,6 @@ using System;
 using System.Data;  
 using System.Data.SqlTypes;  
 using Microsoft.SqlServer.Server;
-//using System.Runtime.InteropServices; // for StructLayout(LayoutKind.Sequential)
 
 public enum ExhaustiveType : ushort { 
     NULL = 0, 
@@ -73,7 +72,6 @@ public static class ExhaustiveTypeFactory {
 }
 
 [Serializable]  
-//[StructLayout(LayoutKind.Sequential)] 
 [Microsoft.SqlServer.Server.SqlUserDefinedType(
     Format.Native,  
     IsByteOrdered = true, 
